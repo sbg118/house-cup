23ce8e670414491c84352564a5f05997dcbfb776
sbg118 <sbg0032@auburn.edu>
2026-07-28 18:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const leaderboard = document.getElementById("leaderboard");
+document.getElementById("leaderboard").innerHTML = `
 
-leaderboard.innerHTML = `
 <div class="card">
-Loading House Standings...
+🦖 RAPTOR HOUSE
+<br><br>
+1250 Points
 </div>
+
+<div class="card">
+🦴 T-REX HOUSE
+<br><br>
+1175 Points
+</div>
+
+<div class="card">
+🌿 TRICERATOPS HOUSE
+<br><br>
+1098 Points
+</div>
+
+<div class="card">
+🥚 BRACHIOSAURUS HOUSE
+<br><br>
+1023 Points
+</div>
+
 `;
+
+document.getElementById("activity").innerHTML = `
+
+<div class="card">
+🏆 Sarah earned 10 points for attending an event.
+</div>
+
+<div class="card">
+🦖 Raptor House moved into 1st place.
+</div>
+
+<div class="card">
+🌋 New Challenge Released!
+</div>
+
+`;
+``
